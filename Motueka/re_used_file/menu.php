35c8e4b20a8fd14386e15c651160c1602e40a027
Wesde66 <wesde66@gmail.com>
2022-12-02 13:32:03 +1300

    the 3 main nav bars are complete

--- Motueka/re_used_file/menu.php
+++ Motueka/re_used_file/menu.php
@@ -1,12 +1,51 @@
 
-    <div id="header">
+    <div id="header" class="w3-bar w3-light-grey">
       <div id="logo">
         <div id="logo_text">
-          <!-- class="logo_colour", allows you to change the colour of the text -->
-          <h1><a href="../index.php"><span class="logo_colour">Ongaonga Bed & Breakfast</span></a></h1>
-          <h2>Make yourself at home is our slogan. We offer some of the best beds on the East Coast. Sleep well and rest well.</h2>
+
+            <a href="../index.php"><span class="w3-bar-item w3-button">Ongaonga Bed & Breakfast</span></a>
+            <a href="../Motueka/login.php" class="w3-bar-item w3-button">Login</a>
+
+            <!--This is the dropdown for Bookings section-->
+            <div class="w3-dropdown-hover">
+                <button class="w3-button">Bookings</button>
+                <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                    <a class="w3-bar-item w3-button" href="../Motueka/bookings/bookingdetailsview.php">View booking details</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/bookings/makebookingandsearchavailability.php">Make booking</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/bookings/makebookingandsearchavailability.php">Search for availability</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/bookings/currentbookings.php">Current bookings</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/bookings/editbooking.php">Edit bookings</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/bookings/delete_bookings.php">Delete a booking</a>
+                </div>
+            </div>
+            <!--This is the dropdown for Rooms section-->
+            <div class="w3-dropdown-hover">
+                <button class="w3-button">Rooms</button>
+                <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                    <a class="w3-bar-item w3-button" href="../Motueka/rooms/addroom.php">Add rooms</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/rooms/deleteroom.php">Delete a room</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/rooms/editoraddroomreview.php">Add or edit a room review</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/rooms/editroom.php">Edit room details</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/rooms/listrooms.php">List all rooms</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/rooms/viewroom.php">View room details</a>
+                </div>
+            </div>
+            <!--This is the dropdown for Customers section-->
+            <div class="w3-dropdown-hover">
+                <button class="w3-button">Customers</button>
+                <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                    <a class="w3-bar-item w3-button" href="../Motueka/customers/customersearch.php">Search for customer</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/customers/deletecustomer.php">Delete customer</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/customers/editcustomer.php">Edit a customer</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/customers/listcustomers.php">List customers</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/customers/registercustomer.php">Register a customer</a>
+                    <a class="w3-bar-item w3-button" href="../Motueka/customers/viewcustomer.php">View customer details</a>
+                </div>
+            </div>
+
+
         </div>
       </div>
     </div>
 
-	
+    <h2>Make yourself at home is our slogan. We offer some of the best beds on the East Coast. Sleep well and rest well.</h2>
