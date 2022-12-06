67a8257ba1b2b9c9ce7c7f29ca263eca91275f7c
Wesde66 <wesde66@gmail.com>
2022-12-06 14:06:10 +1300

    Date picker working

--- Motueka/bookings/makebookingandsearchavailability.php
+++ Motueka/bookings/makebookingandsearchavailability.php
@@ -5,7 +5,7 @@ include "../re_used_file/menu.php";
 
 
 ?>
-<head>
+
 <script>
     function searchResult() {
        var st = document.getElementById('sdate').value;
@@ -15,31 +15,31 @@ include "../re_used_file/menu.php";
         xmlhttp=new XMLHttpRequest();
         xmlhttp.onreadystatechange=function() {
             if (this.readyState==4 && this.status==200) {
-//take JSON text from the server and convert it to JavaScript objects
-//mbrs will become a two-dimensional array of our customers much like
-//a PHP associative array
+                //take JSON text from the server and convert it to JavaScript objects
+                //mbrs will become a two-dimensional array of our customers much like
+                //a PHP associative array
                 var mbrs = JSON.parse(this.responseText);
                 var tbl = document.getElementById("tblcustomers"); //find the table in the HTML
 
-//populate the table
-//mbrs.length is the size of our array
+                    //populate the table
+                    //mbrs.length is the size of our array
                 for (var i = 0; i < mbrs.length; i++) {
                     var mbrid = mbrs[i]['roomID'];
-                    var fn    = mbrs[i]['roomname'];
-                    var ln    = mbrs[i]['roomtype'];
+                    var roomname    = mbrs[i]['roomname'];
+                    var roomtype    = mbrs[i]['roomtype'];
                     var des    = mbrs[i]['description'];
 
-//concatenate our actions urls into a single string
+                    //concatenate our actions urls into a single string
                     var urls  = '<a href='+mbrid+'"viewcustomer.php?id=">[view]</a>';
                     urls += '<a href='+mbrid+'"editcustomer.php?id=">[edit]</a>';
                     urls += '<a href='+mbrid+'"deletecustomer.php?id=">[delete]</a>';
 
-//create a table row with three cells
+                    //create a table row with three cells
                     tr = tbl.insertRow(-1);
                     var tabCell = tr.insertCell(-1);
-                    tabCell.innerHTML = ln; //lastname
+                    tabCell.innerHTML = roomname; //lastname
                     var tabCell = tr.insertCell(-1);
-                    tabCell.innerHTML = fn; //firstname
+                    tabCell.innerHTML = roomtype; //firstname
                     var tabCell = tr.insertCell(-1);
                     tabCell.innerHTML = des; //firstname
                     //var tabCell = tr.insertCell(-1);
@@ -47,33 +47,54 @@ include "../re_used_file/menu.php";
                 }
             }
         }
-//call our php file that will look for a customer or customers matching the search string
+        //The call request to the PHP file for the rooms
         xmlhttp.open("GET","getavailability.php"+string,true);
         xmlhttp.send();
     }
-</script>
-</head>
-<body>
+    $( function() {
+        var st = document.getElementsByName('startdate');
+        $( st ).datepicker({
+            numberOfMonths: 2,
+            showButtonPanel: true,
+            dateFormat: 'yy-mm-dd',
+            minDate: 'today',
 
-<h1>Customer List Search by Lastname</h1>
-<h2><a href='makebookingandsearchavailability.php'>[Create new Customer]</a><a href="../index.php">[Return to main page]</a>
+        });
+    } );
+
+    $( function() {
+        var en = document.getElementsByName('enddate');
+        $( en).datepicker({
+            numberOfMonths: 2,
+            showButtonPanel: true,
+            dateFormat: 'yy-mm-dd',
+            minDate: 'startdate + 1',
+        });
+    } );
+</script>
+
+<body>
+<div>
+    <h1>Please complete the below to make a booking</h1>
+
+</div>
+
+
+<h1>Search for available rooms</h1>
+<h2><a href="http://localhost/Motueka/bookings/currentbookings.php">[Current Bookings]</a><a href="http://localhost/index.php">[Return to main page]</a>
 </h2>
 <form>
-    <label for="lastname">Lastname: </label>
-    <label for="sdate"></label>
-    <input title="E" id="sdate" placeholder="2000-10-20">
-    <label for="edate"></label>
-    <input title="E" id="edate" placeholder="2000-10-20">
+    <label for="checkin">Checkin date: </label>
+
+    <input title="checkin" name="startdate" id="sdate" placeholder="2000-10-20">
+    <label for="checkout">Checkout date: </label>
+    <input title="checkout" id="edate" name="enddate" placeholder="2000-10-20">
 
     <button type="button" name="BTN" onclick="searchResult(this.value)">Fetch available rooms</button>
 
-
-
-
-
 </form>
 <table id="tblcustomers" border="1">
-    <thead><tr><th>Last name</th><th>First name</th><th>Description</th></tr></thead>
+    <thead><tr><th>Room Name</th><th>Room type</th><th>Description</th></tr></thead>
 
 </table>
 </body>
