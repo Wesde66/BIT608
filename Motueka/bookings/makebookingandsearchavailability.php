f2d3b042f1eda49e07eacd05666b7b28a40186be
Wesde66 <wesde66@gmail.com>
2022-12-06 17:47:06 +1300

    search info working with the dates pulling through to the make booking section.

--- Motueka/bookings/makebookingandsearchavailability.php
+++ Motueka/bookings/makebookingandsearchavailability.php
@@ -8,15 +8,20 @@ include "../re_used_file/menu.php";
 
 <script>
     function searchResult() {
+        //Get the date to be sent from the input fields
        var st = document.getElementById('sdate').value;
         var ed = document.getElementById('edate').value;
+        //Copy those dates to the input fields on the make a booking side
+        document.getElementById('stdate').value = st;
+        document.getElementById('endate').value = ed;
+        //Create the string to use with the XMLHTTPrequest
         var string = '?checkInDate='+ st+"&checkOutDate="+ed;
 
         xmlhttp=new XMLHttpRequest();
         xmlhttp.onreadystatechange=function() {
             if (this.readyState==4 && this.status==200) {
                 //take JSON text from the server and convert it to JavaScript objects
-                //mbrs will become a two-dimensional array of our customers much like
+                //mbrs will become a two-dimensional array of our rooms much like
                 //a PHP associative array
                 var mbrs = JSON.parse(this.responseText);
                 var tbl = document.getElementById("tblcustomers"); //find the table in the HTML
@@ -56,6 +61,7 @@ include "../re_used_file/menu.php";
         //The call request to the PHP file for the rooms
         xmlhttp.open("GET","getavailability.php"+string,true);
         xmlhttp.send();
+
     }
     $( function() {
         var st = document.getElementsByName('startdate');
@@ -77,12 +83,14 @@ include "../re_used_file/menu.php";
             minDate: 'startdate + 1',
         });
     } );
+
+
 </script>
 
 <body>
 <div>
     <h2>Please complete the below to make a booking</h2>
-    <h4><a href="http://localhost/Motueka/bookings/currentbookings.php">[Current Bookings]</a><a href="http://localhost/index.php">[Return to main page]</a></h4>
+    <h4><a href="http://localhost/Motueka/bookings/currentbookings.php">[Current Bookings]</a><a href="http://localhost/Motueka/index.php">[Return to main page]</a></h4>
     <br>
     <p>This booking is for user <?php echo $_SESSION['username']; ?></p>
     <section>
@@ -90,6 +98,16 @@ include "../re_used_file/menu.php";
         <select name="RoomNameAvl" id="RoomName" title="RoomName">
         <option id="roomOption" disabled selected value> -- select a room -- </option>
         </select>
+        <br>
+        <label for="stdate">Checkin Date: </label><input type="text" id="stdate" >
+        <label for="endate">Checkout Date: </label><input type="text" id="endate" ><br>
+        <label for="mobile">Mobile number</label><input type="tel" id="mobile">
+        <br>
+        <label for="bookingsExtra">Booking extras :</label><br>
+        <textarea id="bookingsExtra" placeholder="Please let us know if you require anything extra" rows="5" cols="60"></textarea>
+        <br>
+        <button name="Submit" type="button" id="Submit">Confirm booking</button>
+        <button name="Cancel" type="button" value="Reload Page" onclick="window.location.reload();" id="Cancel">Clear booking info</button>
     </section>
 </div>
 
@@ -100,11 +118,11 @@ include "../re_used_file/menu.php";
 <form>
     <label for="checkin">Checkin date: </label>
 
-    <input title="checkin" name="startdate" id="sdate" placeholder="2000-10-20">
+    <input class="startDate" title="checkin" name="startdate" id="sdate" placeholder="2000-10-20">
     <label for="checkout">Checkout date: </label>
     <input title="checkout" id="edate" name="enddate" placeholder="2000-10-20">
 
-    <button type="button" name="BTN" onclick="searchResult(this.value)">Fetch available rooms</button>
+    <button type="button" name="BTN" onclick="searchResult(this.value)" >Fetch available rooms</button>
 
 </form>
 <table id="tblcustomers" border="1">
